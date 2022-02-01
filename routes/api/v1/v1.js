8d708c8c458f09616fb6179f51f387f890bce52e
nicollh9 <nicollarita06@gmail.com>
2022-02-01 14:04:30 -0600

    Token

--- routes/api/v1/v1.js
+++ routes/api/v1/v1.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const {verifyApiHeaderToken} = require('./headerVerifyMiddleware');
 const pacientesRoutes = require('./pacientes/pacientes');
 const expedientesRoutes = require('./expedientes/expedientes');
 
-router.use('/pacientes', pacientesRoutes);
+router.use(
+    '/pacientes',
+    verifyApiHeaderToken, 
+    pacientesRoutes
+);
+
 router.use('/expedientes', expedientesRoutes);
 
 
