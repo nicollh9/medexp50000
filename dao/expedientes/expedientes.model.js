3f9d25036bd4fc662cae92ab0a5e066088f77564
nicollh9 <nicollarita06@gmail.com>
2022-02-18 17:43:56 -0600

    Expedientes faceted and addtag/set

--- dao/expedientes/expedientes.model.js
+++ dao/expedientes/expedientes.model.js
@@ -1,86 +1,110 @@
-const getDb = require('../db')
+const ObjectId = require('mongodb').ObjectId;
+const getDb = require('../mongodb');
 let db = null;
 
 class Expedientes {
+    collection = null;
     constructor() {
         getDb()
             .then((database) => {
                 db = database;
+                this.collection = db.collection('Expedientes');
                 if (process.env.MIGRATE === 'true') {
-                    const createStatement = 'CREATE TABLE IF NOT EXISTS expedientes (id INTEGER PRIMARY KEY AUTOINCREMENT, identidad TEXT, fecha TEXT, descripcion TEXT, observacion TEXT, registros TEXT, ultimaActualizacion TEXT);';
-                    db.run(createStatement);
+                    //
                 }
             })
             .catch((err) => { console.error(err) });
     }
-    new(identidad, fecha, descripcion, observacion, registros, ultimaActualizacion) {
-        return new Promise((accept, reject) => {
-            db.run(
-                'INSERT INTO expedientes (identidad , fecha, descripcion, observacion, registros, ultimaActualizacion) values(?,?,?,?,?,?);', [identidad, fecha, descripcion, observacion, registros, ultimaActualizacion],
-                (err, rslt) => {
-                    if (err) {
-                        console.error(err);
-                        reject(err)
-                    }
-                    accept(rslt);
-                }
-            )
-        })
-    }
-    getAll() {
-        return new Promise((accept, reject) => {
-            db.all('SELECT * FROM expedientes;', (err, rows) => {
-                if (err) {
-                    console.error(err);
-                    reject(err);
-                } else {
-                    accept(rows)
-                }
-            });
-        });
-    }
-    updateOne(id, identidad, fecha, descripcion, observacion, registros, ultimaActualizacion) {
-            return new Promise((accept, reject) => {
-                const sqlUpdate = 'UPDATE expedientes set identidad = ?, fecha = ?, descripcion =  ?, observacion = ? , registros = ?, ultimaActualizacion = ? where id =?;';
-                db.run(
-                    sqlUpdate, [identidad, fecha, descripcion, observacion, registros, ultimaActualizacion, id],
-                    function(err) {
-                        if (err) {
-                            reject(err);
-                        } else {
-                            accept(this)
-                        }
-                    }
-                )
-            })
-        } //UPDATE 
-    getById(id) {
-        return new Promise((accept, reject) => {
-            db.get('SELECT * FROM expedientes where id = ?;', [id], (err, row) => {
-                if (err) {
-                    console.error(err);
-                    reject(err);
-                } else {
-                    accept(row)
-                }
-            });
-        });
-    }
-    deleteOne(id) {
-        return new Promise((accept, reject) => {
-            const sqlDelete = 'DELETE from expedientes where id =?;';
-            db.run(
-                sqlDelete, [id],
-                function(err) {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        accept(this)
-                    }
-                }
-            )
-        })
-    }
-
+    
+//NEW
+  async new(identidad, fecha, descripcion, observacion, registros,ultimaActualizacion) {
+    const newExpediente = {
+      identidad,
+      fecha,
+      descripcion,
+      observacion,
+      registros,
+      ultimaActualizacion
+    };
+    const rslt = await this.collection.insertOne(newExpediente);
+    return rslt;
 }
-module.exports = Expedientes
+
+//GET ALL
+  async getAll() {
+    const cursor = this.collection.find({});
+    const documents = await cursor.toArray();
+    return documents;
+  }
+
+//GET FACETED
+  async getFaceted(page, items, filter = {}) {
+    const cursor = this.collection.find(filter);
+    const totalItems = await cursor.count();
+    cursor.skip((page -1) * items);
+    cursor.limit(items);
+    const resultados = await cursor.toArray();
+    return {
+      totalItems,
+      page,
+      items,
+      totalPages: (Math.ceil(totalItems / items)),
+      resultados
+    };
+  }
+
+//GET BYID
+  async getById(id) {
+    const _id = new ObjectId(id);
+    const filter = {_id};
+    console.log(filter);
+    const myDocument = this.collection.findOne(filter);
+    return myDocument;
+  }
+
+//UPDATE ONE
+async updateOne(id, identidad, fecha, descripcion, observacion, registros, ultimaActualizacion) {
+    const filter = {_id: new ObjectId(id)};
+    const updateCmd = {
+      '$set':{
+        identidad,
+        fecha,
+        descripcion,
+        observacion,
+        registros,
+        ultimaActualizacion
+      }
+    };
+    return await this.collection.updateOne(filter, updateCmd);
+  }
+
+//UPDATE ADD TAG
+  async updateAddTag(id, tagEntry){
+    const updateCmd={
+      "$push":{
+        tags: tagEntry
+      }
+    }
+    const filter = {_id: new ObjectId(id)}
+    return await this.collection.updateOne(filter,updateCmd);
+  }
+
+//UPDATE ADD TAG SET
+  async updateAddTagSet(id, tagEntry){
+    const updateCmd={
+      "$addToSet":{
+        tags: tagEntry
+      }
+    }
+    const filter = {_id: new ObjectId(id)}
+    return await this.collection.updateOne(filter,updateCmd);
+  }
+
+//DELETE
+async deleteOne(id) {
+    const filter = {_id: new ObjectId(id)};
+    return await this.collection.deleteOne(filter);
+  }
+}
+  
+module.exports = Expedientes;
--- routes/api/v1/expedientes/expedientes.js
+++ routes/api/v1/expedientes/expedientes.js
@@ -9,27 +9,9 @@ router.get('/', (req, res) => {
         endpoint: 'Expedienteees',
         updates: new Date(2022, 0, 19, 18, 41, 0)
     })
-}); //GET 
+}); 
 
-router.post('/new', async(req, res) => {
-        const { identidad, fecha, descripcion, observacion, registros, ultimaActualizacion } = req.body;
-        try {
-            rslt = await expedienteModel.new(identidad, fecha, descripcion, observacion, registros, ultimaActualizacion)
-            res.status(200).json({
-                status: 'ok',
-                received: rslt
-            })
-        } catch (ex) {
-            console.log(ex);
-            res.status(502).json({
-                status: 'failed',
-                result: {}
-            })
-        }
-
-    }) //POST / new
-
-/***All***/
+//GET ALL
 router.get('/all', async(req, res) => {
         try {
             const rows = await expedienteModel.getAll();
@@ -38,53 +20,125 @@ router.get('/all', async(req, res) => {
             console.log(ex);
             res.status(500).json({ status: 'failed' });
         }
-    }) //get All
+    }) 
 
-/***Update***/
-router.put('/update/:id', async(req, res) => {
+// BY ID
+router.get('/byid/:id', async (req, res) => {
     try {
-        const { identidad, fecha, descripcion, observacion, registros, ultimaActualizacion } = req.body;
-        const { id } = req.params;
-        const result = await expedienteModel.updateOne(id, identidad, fecha, descripcion, observacion, registros, ultimaActualizacion);
-        res.status(200).json({
-            status: 'ok',
-            result
-        })
+      const { id } = req.params;
+      const row = await expedienteModel.getById(id);
+      res.status(200).json({ status: 'ok', expediente: row });
     } catch (ex) {
+      console.log(ex);
+      res.status(500).json({ status: 'failed' });
+    }
+  });
+  
+
+const allowedItemsNumber = [10, 15, 20];
+
+//FACET SEARCH
+router.get('/facet/:page/:items', async (req, res) => {
+    const page = parseInt(req.params.page, 10);
+    const items = parseInt(req.params.items, 10);
+    if (allowedItemsNumber.includes(items)) {
+      try {
+        const expedientes = await expedienteModel.getFaceted(page, items);
+        res.status(200).json({docs:expedientes});
+      } catch (ex) {
         console.log(ex);
         res.status(500).json({ status: 'failed' });
+      }
+    } else {
+      return res.status(403).json({status:'error', msg:'Not a valid item value (10,15,20)'});
     }
-})
+  
+  });
+  
 
-/***By id***/
-router.get('/byid/:id', async(req, res) => {
-        try {
-            const { id } = req.params;
-            const rows = await expedienteModel.getById(parseInt(id));
-            res.status(200).json({ status: 'ok', expedientes: rows })
-        } catch (ex) {
-            console.log(ex);
-            res.status(500).json({ status: 'failed' });
-        }
-    }) //get byId
+//ROUTER.POST
+router.post('/new', async (req, res) =>{
+  const {identidad, fecha, descripcion, observacion, registro, ultimoActualizacion} = req.body;
+  try {
+      rslt = await expedienteModel.new(identidad, fecha, descripcion, observacion, registro,ultimoActualizacion);
+      res.status(200).json(
+          {
+              status: 'ok',
+              result: rslt
+          }
+      );
+  } catch (ex) {
+      console.log(ex);
+      res.status(500).json(
+          {
+              status: 'failed',
+              result: {}
+          }
+      );
+  }
+});
 
-/***Delete***/
-router.delete('/delete/:id', async(req, res) => {
-        try {
-            const { id } = req.params;
-            const result = await expedienteModel.deleteOne(id);
-            res.status(200).json({
-                status: 'ok',
-                result
-            })
-        } catch (ex) {
-            console.log(ex);
-            res.status(500).json({ status: 'failed' });
-        }
-    }) //DELETE
+  //ROUTER.PUT
+router.put('/update/:id', async (req, res) => {
+    try{
+      const { identidad, fecha, descripcion, observacion, registros,ultimaActualizacion } = req.body;
+      const { id } = req.params;
+      const result = await expedienteModel.updateOne(id, identidad, fecha, descripcion, observacion, registros,ultimaActualizacion);
+      res.status(200).json({
+        status:'ok',
+        result
+      });
+    } catch(ex){
+      console.log(ex);
+      res.status(500).json({ status: 'failed' });
+    }
+  });
 
-// router.post();
-// router.put();
-// router.delete();
+  //ADD TAG
+router.put('/addtag/:id', async (req, res) => {
+    try{
+      const { tag } = req.body;
+      const { id } = req.params;
+      const result = await expedienteModel.updateAddTag(id, tag);
+      res.status(200).json({
+        status:'ok',
+        result
+      });
+    } catch(ex){
+      console.log(ex);
+      res.status(500).json({ status: 'failed' });
+    }
+  });
+  
+ //ADD TAG SET
+  router.put('/addtagset/:id', async (req, res) => {
+    try{
+      const {tag } = req.body;
+      const { id } = req.params;
+      const result = await expedienteModel.updateAddTagSet(id, tag);
+      res.status(200).json({
+        status:'ok',
+        result
+      });
+    } catch(ex){
+      console.log(ex);
+      res.status(500).json({ status: 'failed' });
+    }
+  });
 
-module.exports = router;
+//ROUTER.DELETE
+router.delete('/delete/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      const result = await expedienteModel.deleteOne(id);
+      res.status(200).json({
+        status: 'ok',
+        result
+      });
+    } catch (ex) {
+      console.log(ex);
+      res.status(500).json({ status: 'failed' });
+    }
+  });
+
+  module.exports = router;
