cb0bb4004ad056e3ced6a278569415376004e08b
nicollh9 <nicollarita06@gmail.com>
2022-02-27 20:30:35 -0600

    new routers

--- routes/api/v1/v1.js
+++ routes/api/v1/v1.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const {verifyApiHeaderToken} = require('./headerVerifyMiddleware');
-const pacientesRoutes = require('./pacientes/pacientes');
-const expedientesRoutes = require('./expedientes/expedientes');
+const { verifyApiHeaderToken } = require('./headerVerifyMiddleware');
+const {passport, jwtMiddleware} = require('./seguridad/jwtHelper');
 
+const pacientesRoutes = require('./pacientes/pacientes');
+const seguridadRoutes = require('./seguridad/seguridad');
+const expedientesRoutes = require('./expedientes/expedientes');
+router.use(passport.initialize());
+
+//public
+router.use('/seguridad', 
+  verifyApiHeaderToken, 
+  seguridadRoutes);
+
+//middlewares
 router.use(
-    '/pacientes',
-    verifyApiHeaderToken, 
-    pacientesRoutes
+  '/pacientes',
+  verifyApiHeaderToken,
+  jwtMiddleware,
+  pacientesRoutes
 );
 
 router.use(
     '/expedientes', 
     verifyApiHeaderToken, 
     expedientesRoutes);
-
 
 module.exports = router;
